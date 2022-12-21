00c88566cb71adb6852c71ba11a62f8ae976d54f
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2022-12-21 15:20:59 +0100

    fix design and

---
+++ main.js
@@ -0,0 +1,21 @@
+$('.neumorphic-checkbox').on('click', function () {
+      $(this).toggleClass('neumorphic-checkbox_active');
+});
+$("#questions").hide();
+function start() {
+      $("#welcome").hide();
+      $("#questions").show();
+      var timeLeft = 30;
+      var elem = $("#timer");
+      var timerId = setInterval(countdown, 1000);
+
+      function countdown() {
+            if (timeLeft == -1) {
+                  clearTimeout(timerId);
+                  // doSomething();
+            } else {
+                  elem.text(timeLeft + ' s');
+                  timeLeft--;
+            }
+      }
+}
