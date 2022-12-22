9413967c6cef157f0c577146f117ce2c70d734e9
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-12-22 23:09:06 +0100

    start fetching data from json

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-$('.neumorphic-checkbox').on('click', function () {
-      $(this).toggleClass('neumorphic-checkbox_active');
+var timerId
+$.getJSON('data.json', function (data) {
+      data.forEach(item => {
+            setInterval(function () {
+                  $("#question").text(item.question);
+                  // $("#rep1").html(data);
+                  // $("#rep2").html(data);
+                  // $("#rep3").html(data);
+                  // $("#rep4").html(data);
+
+            }, 1000);
+
+            // timer()
+      })
+
 });
-$("#questions").hide();
+
+function fetching(data) {
+      $("#question").html(data.question);
+      // $("#rep1").html(data);
+      // $("#rep2").html(data);
+      // $("#rep3").html(data);
+      // $("#rep4").html(data);
+
+}
+
+$("#welcome").hide();
 function start() {
       $("#welcome").hide();
       $("#questions").show();
-      var timeLeft = 30;
+      timer()
+}
+function timer() {
+      var timeLeft = 12;
       var elem = $("#timer");
       var timerId = setInterval(countdown, 1000);
 
@@ -18,3 +44,8 @@ function start() {
             }
       }
 }
+
+
+$('.neumorphic-checkbox').on('click', function () {
+      $(this).toggleClass('neumorphic-checkbox_active');
+});
