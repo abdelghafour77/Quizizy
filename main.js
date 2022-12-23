9786d656a3b8ced8162a792737bbc20ae9da610e
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-12-24 00:23:21 +0100

    try to save data from user

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 let index = 0;
 let data;
 var timerId
+var user_answers = []
+var answer = {}
 
 $.getJSON('data.json', function (jsonData) {
       data = jsonData;
 });
 
 $('#next').click(function () {
+      answer.answer1 = $('#answer1').hasClass('neumorphic-checkbox_active')
+      answer.answer2 = $('#answer2').hasClass('neumorphic-checkbox_active')
+      answer.answer3 = $('#answer3').hasClass('neumorphic-checkbox_active')
+      answer.answer4 = $('#answer4').hasClass('neumorphic-checkbox_active')
+      user_answers.push(answer)
+      console.log(user_answers)
       clearInterval(timerId);
       timer()
+      clearAnswers()
       fetching()
       if (index >= data.length) {
             index = 0;
       }
 });
+function clearAnswers() {
+      $('#answer1').removeClass('neumorphic-checkbox_active')
+      $('#answer2').removeClass('neumorphic-checkbox_active')
+      $('#answer3').removeClass('neumorphic-checkbox_active')
+      $('#answer4').removeClass('neumorphic-checkbox_active')
+}
 function fetching() {
       $('#question').html(data[index].question);
-      $('#answer1').html(data[index].answers[0].answer);
-      $('#answer2').html(data[index].answers[1].answer);
-      $('#answer3').html(data[index].answers[2].answer);
-      $('#answer4').html(data[index].answers[3].answer);
+      $('#answer1 p').html(data[index].answers[0].answer);
+      $('#answer2 p').html(data[index].answers[1].answer);
+      $('#answer3 p').html(data[index].answers[2].answer);
+      $('#answer4 p').html(data[index].answers[3].answer);
       index++;
 }
 
@@ -50,3 +65,5 @@ function timer() {
 $('.neumorphic-checkbox').on('click', function () {
       $(this).toggleClass('neumorphic-checkbox_active');
 });
+
+
