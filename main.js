d8d4f9a58c5592f8fcf1dbbea322ff061fb36a4c
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-12-24 15:11:25 +0100

    add progress status and name

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ var timerId
 var user_answers = []
 var answer = {}
 
+$("#questions").hide();
+
+$('.neumorphic-checkbox').on('click', function () {
+      $(this).toggleClass('neumorphic-checkbox_active');
+});
+
 $.getJSON('data.json', function (jsonData) {
       data = jsonData;
 });
 
 $('#next').click(function () {
+      core();
+});
+
+function core() {
       answer.answer1 = $('#answer1').hasClass('neumorphic-checkbox_active')
       answer.answer2 = $('#answer2').hasClass('neumorphic-checkbox_active')
       answer.answer3 = $('#answer3').hasClass('neumorphic-checkbox_active')
@@ -19,16 +29,19 @@ $('#next').click(function () {
       timer()
       clearAnswers()
       fetching()
+      $("#progress").text(index + '/' + data.length)
       if (index >= data.length) {
             index = 0;
       }
-});
+}
+
 function clearAnswers() {
       $('#answer1').removeClass('neumorphic-checkbox_active')
       $('#answer2').removeClass('neumorphic-checkbox_active')
       $('#answer3').removeClass('neumorphic-checkbox_active')
       $('#answer4').removeClass('neumorphic-checkbox_active')
 }
+
 function fetching() {
       $('#question').html(data[index].question);
       $('#answer1 p').html(data[index].answers[0].answer);
@@ -36,24 +49,25 @@ function fetching() {
       $('#answer3 p').html(data[index].answers[2].answer);
       $('#answer4 p').html(data[index].answers[3].answer);
       index++;
+      timer()
 }
 
-$("#questions").hide();
 function start() {
       $("#welcome").hide();
       $("#questions").show();
       fetching()
 }
+
 function timer() {
       let timeLeft = 30;
       var elem = $("#timer");
       clearInterval(timerId)
+      elem.text(timeLeft + ' s');
+      timeLeft -= 1;
       timerId = setInterval(function () {
 
             if (timeLeft == -1) {
-                  clearInterval(timerId);
-                  timer()
-                  fetching()
+                  core()
             } else {
                   elem.text(timeLeft + ' s');
                   timeLeft -= 1;
@@ -61,9 +75,5 @@ function timer() {
       }, 1000);
 }
 
-
-$('.neumorphic-checkbox').on('click', function () {
-      $(this).toggleClass('neumorphic-checkbox_active');
-});
 
 
